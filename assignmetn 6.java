1122c28051ab836e6797f8b096edead41673f04c
Mark <72723296+markl12s@users.noreply.github.com>
2021-10-19 10:04:39 -0700

    Create assignmetn 6.java

---
+++ assignmetn 6.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //recieve weight of package
+        System.out.println("please input weight of package");
+        Scanner in = new Scanner(System.in);
+        String weightString = in.nextLine();
+
+        //recieve size of package
+        System.out.println("please input size of package, in cubic feet");
+        in = new Scanner(System.in);
+        String sizeString = in.nextLine();
+
+        //turn strings into variables
+        int weightInt = Integer.parseInt(weightString);
+        int sizeInt = Integer.parseInt(sizeString);
+
+        //comparison
+        if(weightInt > 99){
+            System.out.println("Too heavy");
+        }
+        if(sizeInt > 10){
+            System.out.println("Too big");
+        }
+    }
+}
